0a13075da14f3f73ec1fff79ac067eb43bb7a43d
Felix Ocker <felix.ocker@googlemail.com>
2021-07-13 23:54:12 +0200

    add license header to individual files

--- example/example.py
+++ example/example.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 """minimal example for applying the ontor module"""
 
+#
+# This file is part of ontor (https://github.com/felixocker/ontor).
+# Copyright (c) 2021 Felix Ocker.
+#
+# ontor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ontor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ontor.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 import ontor
 
 def create_first_onto():

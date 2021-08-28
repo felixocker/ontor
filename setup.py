3cee6d62e3619858415828b1ba8b89f661b78b43
Felix Ocker <felix.ocker@googlemail.com>
2021-08-28 17:00:25 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ontor",
-    version="0.0.1",
+    version="0.1.0",
     author="Felix Ocker",
     author_email="felix.ocker@googlemail.com",
     description="ontor - an ontology editor based on Owlready2",

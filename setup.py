97a47d2e73dd9023e53369632f632eafbf9c88b2
Felix Ocker <felix.ocker@googlemail.com>
2022-06-15 16:25:55 +0200

    build: bump version number for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ontor",
-    version="0.4.0",
+    version="0.4.1",
     author="Felix Ocker",
     author_email="felix.ocker@googlemail.com",
     description="ontor - an ontology editor based on Owlready2",
@@ -16,7 +16,7 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://github.com/felixocker/ontor/issues",
     },
-    download_url="https://github.com/felixocker/ontor/archive/refs/tags/v0.4.0.tar.gz",
+    download_url="https://github.com/felixocker/ontor/archive/refs/tags/v0.4.1.tar.gz",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",

3fcac337ee0bdb14d32c98ec3aa5bd5cf7d823c2
Felix Ocker <felix.ocker@googlemail.com>
2022-08-06 19:25:47 +0200

    feat: extend grammar for properties, add examples

--- src/ontor/manchester_parser.py
+++ src/ontor/manchester_parser.py
@@ -7,10 +7,11 @@ NOTE: at the time of writing, this is not meant to be a fully functional parser 
 for single axioms expressed in Manchester Syntax
 """
 
-from lark import Lark
+from lark import Lark, Tree, Transformer
+import typing
 
 
-def parse(grammar_file: str, axiom: str):
+def parse(grammar_file: str, axiom: str) -> typing.Union[Tree, Tree]:
     with open(grammar_file, "r") as g:
         grammar = g.read()
     lark_parser = Lark(grammar)
@@ -20,7 +21,10 @@ def parse(grammar_file: str, axiom: str):
 if __name__ == "__main__":
     lark_grammar_file = "./config/manchester_syntax.config"
     example_axiom_1 = "pizza EquivalentTo not ( has min 1 topping and not ( has exactly 1 base ) )"
-    example_axiom_2 = "pizza SubClassOf Inverse ( has min 1 topping and has only base )"
-    example_axiom_3 = "pizza SubClassOf diam only xsd:integer [ > 0 , < 5]"
-    for ax in example_axiom_1, example_axiom_2:
-        print(parse(lark_grammar_file, ax))
+    example_axiom_2 = "pizza SubClassOf Inverse ( has ) min 1 topping and has only base"
+    example_axiom_3 = "Inverse ( has ) min 2 topping and has only base SubClassOf Inverse ( has ) min 1 topping and has only base"
+    example_axiom_4 = "pizza SubClassOf diam only xsd:integer [ > 0 , < 5]"
+    for ax in example_axiom_1, example_axiom_2, example_axiom_3, example_axiom_4:
+        tree = parse(lark_grammar_file, ax)
+        print(tree)
+        print(tree.pretty())

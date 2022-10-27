9761615f7bc42b788e6fbb48d7c78cedc426ae82
Felix Ocker <felix.ocker@googlemail.com>
2022-10-27 16:06:38 +0200

    chore: bump version

--- src/ontor/_about.py
+++ src/ontor/_about.py
@@ -3,7 +3,7 @@
 __name__ = "ontor"
 __author__ = "Felix Ocker"
 __author_email__ = "felix.ocker@googlemail.com"
-__version__ = "0.4.7"
+__version__ = "0.4.8"
 __url__ = "https://github.com/felixocker/ontor"
 __download_url__ = "https://github.com/felixocker/ontor/archive/refs/tags/v" + __version__ + ".tar.gz"
 __keywords__ = ["ontology", "owl", "python", "knowledge graph"]

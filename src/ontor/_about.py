a4cf55647466c95a554718db81fbf8687827a0b0
Felix Ocker <felix.ocker@googlemail.com>
2022-11-17 12:40:53 +0100

    chore: bump version

--- src/ontor/_about.py
+++ src/ontor/_about.py
@@ -3,7 +3,7 @@
 __name__ = "ontor"
 __author__ = "Felix Ocker"
 __author_email__ = "felix.ocker@googlemail.com"
-__version__ = "0.4.8"
+__version__ = "0.4.9"
 __url__ = "https://github.com/felixocker/ontor"
 __download_url__ = (
     "https://github.com/felixocker/ontor/archive/refs/tags/v" + __version__ + ".tar.gz"
